d3a4c0245cde63955e6eb8b19205040251835d3e
Villads Valur Korsholm Nielsen <villadsvalur@gmail.com>
2017-01-10 20:23:25 +0100

    tilføjede nye assets, fjernede gamle

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,7 @@
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './modules/app.module';
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 enableProdMode();
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
